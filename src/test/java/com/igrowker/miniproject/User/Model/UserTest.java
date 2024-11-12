d3462c567a1c58fffb610df0054222c46e08be6b
Kevin Felipe Bolaños Orozco <kevinboor5725@gmail.com>
2024-11-12 16:38:38 -0500

    implemented user model test

--- src/test/java/com/igrowker/miniproject/User/Model/UserTest.java
+++ src/test/java/com/igrowker/miniproject/User/Model/UserTest.java
@@ -1,4 +1,26 @@
 package com.igrowker.miniproject.User.Model;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class UserTest {
+    // Test cases for User model
+
+    @Test
+    @DisplayName("Should create user with valid data")
+    public void shouldCreateUser(){
+        // Given
+        User user = new User("John Doe", "john@gmail.com", "John123.", false, "USA");
+
+        // Then
+        assertNotNull(user, "User should not be null");
+        assertEquals("John Doe", user.getName());
+        assertEquals("john@gmail.com", user.getEmail());
+        assertEquals("John123.", user.getPassword());
+        assertEquals(false, user.isAdmin());
+        assertEquals("USA", user.getCountry());
+    }
 }
