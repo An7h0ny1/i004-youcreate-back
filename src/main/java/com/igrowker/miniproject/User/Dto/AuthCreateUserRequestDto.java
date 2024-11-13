c23801e50fa26e39fb2d00598e463b3628375b86
An7h0ny1 <91570972+An7h0ny1@users.noreply.github.com>
2024-11-13 14:12:09 -0500

    fix fields of userEntity

--- src/main/java/com/igrowker/miniproject/User/Dto/AuthCreateUserRequestDto.java
+++ src/main/java/com/igrowker/miniproject/User/Dto/AuthCreateUserRequestDto.java
@@ -1,11 +1,13 @@
 package com.igrowker.miniproject.User.Dto;
 
-import jakarta.validation.Valid;
+import com.igrowker.miniproject.User.Model.Enum.EnumCountry;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record AuthCreateUserRequestDto(@NotBlank(message = "Username cannot be blank") String userName,
                                        @NotBlank(message = "Last name cannot be blank") String lastName,
                                        @NotBlank(message = "Email cannot be blank") @Email(message = "Email must be valid") String email,
-                                       @NotBlank(message = "Password cannot be blank") String password) {
+                                       @NotBlank(message = "Password cannot be blank") String password,
+                                       @NotNull(message = "Country cannot be null") EnumCountry country) {
 }
