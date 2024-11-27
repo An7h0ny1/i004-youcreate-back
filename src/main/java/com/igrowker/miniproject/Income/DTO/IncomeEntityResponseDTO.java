66d0930348bb601084a28002cdb3181ac88abdd6
Stackft <mauriciofst04@gmail.com>
2024-11-26 21:23:44 -0300

    fix verify

--- src/main/java/com/igrowker/miniproject/Income/DTO/IncomeEntityResponseDTO.java
+++ src/main/java/com/igrowker/miniproject/Income/DTO/IncomeEntityResponseDTO.java
@@ -3,7 +3,7 @@ package com.igrowker.miniproject.Income.DTO;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Data;
 
-import java.util.Date;git
+import java.util.Date;
 
     @JsonPropertyOrder({"id", "amount", "origin", "date", "category", "user_id"})
     public record IncomeEntityResponseDTO(
